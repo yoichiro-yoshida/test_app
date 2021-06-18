c51b0ea2a22cced7a6cf0472de14c9853bf6db13
yoichiro-yoshida <2udonnoko11@gmail.com>
2021-06-18 11:22:44 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text('I am FriendsTree Teem'),
+            ),
+            body: Center(
+              child:Text(
+                 'Holle World',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize:55,
+                ),
+              ),
+              //child:Image(
+                //image: AssetImage('image/FrendsTree_member.JPG'),
+              //),
+            ),
+          ),
+      ),
+  );
+}
